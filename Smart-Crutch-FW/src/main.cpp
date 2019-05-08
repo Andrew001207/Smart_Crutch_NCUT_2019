7f22117ade86ca4eec5c7697f64bcdaf4f0b6cf8
Andrew001207 <andrey001207@gmail.com>
2019-05-08 18:59:33 +0800

    Vibration function added

--- Smart-Crutch-FW/src/main.cpp
+++ Smart-Crutch-FW/src/main.cpp
@@ -6,6 +6,10 @@
     Vl53L1_PWM sensor_C(PB_12);
     Vl53L1_PWM sensor_D(PB_14);
     Vl53L1_PWM sensor_E(PB_13);
+
+    DigitalOut m_1 (PA_10, 0);
+    DigitalOut m_2 (PA_11, 0);
+
     uint16_t values[5];
   DigitalOut led (PC_13, 1);
   Serial pc(PA_2, PA_3,115200);
@@ -18,27 +22,20 @@ int main() {
   // put your setup code here, to run once
 
   while(1) {
-
     
     for (int i = 0; i < 5; i++)
         pc.printf("%i\t", values[i]);
     pc.printf("\r\n");
-    wait(0.1);
+    //wait(0.1);
     UpdateSensors();
-    
-    //sensor.getRange();
-//    int x = sensor.getRange();
-    // if (x <= 300)
-    //   {
-    //     led = 0;
-    //   }
-    // else
-    //   {
-    //     led = 1;
-    //   }
-    // pc.printf("%i\r\n", x);
-    
-    // put your main code here, to run repeatedly:
+    bool vibration = false;
+    for (int i = 0; i < 5; i++)
+    {
+      if(values[i] <= safe_distance)
+      vibration = true;
+    }
+    m_1 = vibration;
+    m_2 = vibration;
   }
 }
 
